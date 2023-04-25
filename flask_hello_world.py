03ed6fb2413ea50f42722a05d3297eb4a4c2a824
GhostWheat <32331476+GhostWheat@users.noreply.github.com>
2023-04-24 20:52:40 -0500

    first commit

--- flask_hello_world.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask #remember, capital letters used for a Class!
-app = Flask(__name__) # so we set app = an instance of a flask Class, passing in
-
-
-@app.route("/") #homepage of website
-def hello():
-    return "<h1>Hello World in Flask!</h1>"
